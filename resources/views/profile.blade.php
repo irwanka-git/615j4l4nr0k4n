512b2f2cfb3e6552c6071fda152beb925baab48e
Irwan Kurniawan <drive.irwanka@gmail.com>
2023-02-09 23:19:37 +0700

    VISI MISI

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,18 +1,37 @@
 <div class="row">
   <center class="mt-2 p-2">
-      <h4>Profil Dinas Pekerjaan Umum dan Penataan Ruang Kabupaten Rokan Hulu</h4>
+    <h4>Profil Dinas Pekerjaan Umum dan Penataan Ruang Kabupaten Rokan Hulu</h4>
   </center>
   <div class="col-12 px-5 ">
-        <img width="100%" class="img img-responsive" src="{{asset('img/dinas-pu-banner.jpg')}}">
-        <br>
-        <br>
-        <br>
-        <table>
-            <tr>
-                <td>
-                    <h4>VISI MISI :<h4>
-                </td>
+    <img width="100%" class="img img-responsive" src="{{asset('img/dinas-pu-banner.jpg')}}">
+    <br>
+    <br>
+    <br>
+    <table>
+      <tr>
+        <td>
+          <h4>VISI MISI :<h4>
+            <b>Visi</b>
+            <p>Dalam pelaksanaan tugas dan fungsi dari Dinas PUPRPKPP Provinsi Riau mengacu kepada visi Gubernur Riau yang tertuang pada Rencana Pembangunan Daerah Jangka Menengah (RPJMD) 2019-2024, adapun Visi Gubernur Riau adalah Terwujudnya Riau yang Berdaya Saing, Sejahtera, Bermartabat dan Unggul di Indonesia.</p>
+
+            <b>Misi</b>
+            <p>Sementara Misi adalah sesuatu yang harus diemban atau dilaksanakan organisasi, sesuai visi yang telah ditetapkan, agar tujuan organisasi dapat terlaksana dan berhasil dengan baik. Misi Gubernur terpilih memperlihatkan secara jelas tahapan yang penting dalam proses pembangunan di Provinsi Riau, dari 5 (lima) Misi Gubernur Riau terdapat 4 (empat) Misi yang dilaksanakan oleh Dinas PUPRPKPP Provinsi Riau adalah sebagai berikut :<p>
+              <ol>
+                <li>
+                  Misi 1 : Mewujudkan Sumber Daya Yang Beriman, Berkualitas dan Berdaya Saing  Global Melalui Pembangunan Manusia 
+                  Seutuhnya.
+                </li>
+                <li> 
+                  Misi 2 : Mewujudkan Pembangunan Infrastruktur Daerah Yang Merata dan Berwawasan Lingkungan.
+                </li>
+                <li> 
+                  Misi 3 : Mewujudkan Pembangunan Ekonomi Yang Inklusif, Mandiri dan Berdaya Saing.
+                </li>
+                <li> 
+                  Misi 5 : Mewujudkan Tata Kelola Pemerintahan Yang Baik dan Pelayanan Publik Yang Prima Berbasis Teknologi Informasi. 
+                </li>
+              </td>
             </tr>
-        </table>
-  </div>
-</div>
+          </table>
+        </div>
+      </div>
